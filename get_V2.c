c56df9f8618562c9fb52b7bd294b4e77c839c591
Senkyano <91433801+Senkyano@users.noreply.github.com>
2023-11-22 17:42:38 +0100

    V2

--- get_V2.c
+++ get_V2.c
@@ -1,23 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rihoy <rihoy@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/22 08:33:41 by rihoy             #+#    #+#             */
-/*   Updated: 2023/11/22 17:35:29 by rihoy            ###   ########.fr       */
+/*   Created: 2023/11/13 09:48:44 by rihoy             #+#    #+#             */
+/*   Updated: 2023/11/13 09:48:46 by rihoy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
-
-int	main()
-{
-	int fd = open("lp.txt", O_RDONLY);
-
-	printf(" 1 %s",get_next_line(fd));
-	printf(" 2 %s",get_next_line(fd));
-	close(fd);
-	return (0);
-}
--- get_next_line.h
+++ get_next_line.h
@@ -1,16 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rihoy <rihoy@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/13 09:46:34 by rihoy             #+#    #+#             */
+/*   Updated: 2023/11/22 13:43:32 by rihoy            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <bsd/string.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <string.h>
 
-char	*ft_strjoin(const char *str1, const char *str2);
-size_t	ft_strlen(const char *str);
-int		ft_srchcara(const char *str, int c);
-char	*ft_cpyrest(char *str);
 char	*get_next_line(int fd);
-char	*ft_strjointo(char *s1, char *s2, ssize_t to);
+char	*ft_malloc(char *cpy, ssize_t len);
+char	*ft_to_n(char *all, char *buf, ssize_t totR);
 
 #endif
